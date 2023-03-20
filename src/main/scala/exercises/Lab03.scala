9fcc5dc18d5bdf44b1813808935be85f45ed74e9
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-03-20 12:16:24 +0100

    implement 1a

--- src/main/scala/exercises/Lab03.scala
+++ src/main/scala/exercises/Lab03.scala
@@ -8,8 +8,9 @@ object Lab03 extends App:
   object List:
     @scala.annotation.tailrec
     def drop[A](l: List[A], n: Int): List[A] = (l, n) match
-      case (Nil(), _) => l
-      case (l, n) if n < 0 => l
-      case (Cons(_, t), 0) => t
+      case(l, 0) => l
+      case(l, i) if i < 0 => l
       case (Cons(_, t), i) => drop(t, i-1)
+      case _ => Nil()
+
 
--- src/test/scala/exercises/TestList.scala
+++ src/test/scala/exercises/TestList.scala
@@ -2,14 +2,15 @@ package exercises
 import exercises.Lab03.List.*
 import org.junit.*
 import org.junit.Assert.*
+import exercises.Lab03.*
 
 class TestList:
   val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
   
   @Test def testDrop() =
-    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 0))
-    assertEquals(Cons(10, Cons(30, Nil())), drop(l, 1))
-    assertEquals(Cons(10, Cons(20, Nil())), drop(l, 3))
-    assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), drop(l, 4))
+    assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), drop(l, 0))
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 1))
+    assertEquals(Cons(30, Nil()), drop(l, 2))
+    assertEquals(Nil(), drop(l, 4))
     assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), drop(l, -1))
     
