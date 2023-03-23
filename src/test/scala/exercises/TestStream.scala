9e251d3d06006bb015ff7ceefbdaf30e91a783d0
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-03-23 11:44:43 +0100

    implement drop and costant

---
+++ src/test/scala/exercises/TestStream.scala
@@ -0,0 +1,21 @@
+package exercises
+import org.junit.*
+import org.junit.Assert.*
+import Lab03.Stream
+import Lab03.List.*
+
+class TestStream {
+
+  val s = Stream.take(Stream.iterate(0)(_ + 1))(10)
+
+
+  @Test def testDrop() =
+    assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.take(Stream.drop(s)(6))(10)))
+    assertEquals(Nil(), Stream.toList(Stream.take(Stream.drop(s)(10))(10)))
+    assertEquals(Nil(), Stream.toList(Stream.take(Stream.drop(s)(11))(10)))
+
+  @Test def testCostant() =
+    assertEquals(Cons(1, Cons(1, Cons(1, Cons(1, Nil())))), Stream.toList(Stream.take(Stream.constant(1))(4)))
+    assertEquals(Cons(2, Cons(2, Cons(2, Cons(2, Nil())))), Stream.toList(Stream.take(Stream.constant(2))(4)))
+    assertEquals(Cons(3, Cons(3, Cons(3, Cons(3, Nil())))), Stream.toList(Stream.take(Stream.constant(3))(4)))
+}
